197b3db972abb339ce85dc159b5465c8c99e9c88
Pruthvi Patel <pruthvipatel161001@gmail.com>
2025-02-09 21:36:58 -0500

    second commit

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -5,21 +5,9 @@ const Announcements = () => {
         <h1 className="text-xl font-semibold">Announcements</h1>
         <span className="text-xs text-gray-400">View All</span>
       </div>
-      <div className="flex flex-col gap-4 mt-4">
-        <div className="bg-lamaSkyLight rounded-md p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="font-medium">All announcements</h2>
-            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
-              2025-01-01
-            </span>
-          </div>
-          <p className="text-sm text-gray-400 mt-1">
-            ...
-          </p>
-        </div>
         <div className="bg-lamaPurpleLight rounded-md p-4">
           <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <h2 className="font-medium">Feb 17 2025 will be holiday</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
               2025-01-01
             </span>
@@ -30,13 +18,25 @@ const Announcements = () => {
         </div>
         <div className="bg-lamaYellowLight rounded-md p-4">
           <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <h2 className="font-medium">Midterm Exam starts on 25th Feb 2025</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
               2025-01-01
             </span>
           </div>
           <p className="text-sm text-gray-400 mt-1">
             ....
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 mt-4">
+        <div className="bg-lamaSkyLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Reading week will be from 3rd to 9th March 2025 for this term</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            ...
           </p>
         </div>
       </div>
